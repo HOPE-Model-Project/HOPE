f45ca513bd41b72f98bf3965be11aa89b7983c8b
zoe667 <zysong16@outlook.com>
2024-04-05 15:09:57 -0400

    writeout method error revised

--- src/plot_output_operation.jl
+++ src/plot_output_operation.jl
@@ -1,7 +1,7 @@
 using DataFrames, CSV, PlotlyJS
 
-input_dir = "D:\\Coding\\Master\\HOPE\\ModelCases\\PJM_GTEP_case\\Output\\" # Please change it to your home directory where HOPE and your Output file of the ModelCases exist
-outpath = "D:\\Coding\\Master\\HOPE\\ModelCases\\PJM_GTEP_case\\Output\\"
+input_dir = "D:\\Coding\\Master\\HOPE\\ModelCases\\PJM_PCM_case\\Output\\" # Please change it to your home directory where HOPE and your Output file of the ModelCases exist
+outpath = "D:\\Coding\\Master\\HOPE\\ModelCases\\PJM_PCM_case\\"
 
 #Function use for aggregrating generation data:
 function aggregate_capdata(df)
@@ -38,19 +38,11 @@ color_map = Dict(
 ordered_tech = ["NGCC","NuC","Coal","NGCT","Landfill_NG", "NG", "Hydro","Oil","MSW","Bio","WindOn","SolarPV","Battery","Other"]
 
 #read output data#
-#capacity
-Output_capacity=CSV.read(input_dir*"capacity.csv",DataFrame)
-Output_es_capacity=CSV.read(input_dir*"es_capacity.csv",DataFrame)
-#power
-#Output_power= CSV.read(input_dir*"power_hourly.csv",DataFrame) ###
-#Output_es_power =  CSV.read(input_dir*"es_power_hourly.csv",DataFrame) ###
-#Existing 
-Exist_capacity = filter(row -> row.EC_Category  == "Existing", Output_capacity)
-Exist_es_capacity = filter(row -> row.EC_Category  == "Existing", Output_es_capacity)
 
-#New_Build
-New_capacity = filter(row -> row.New_Build == 1, Output_capacity)
-New_es_capacity = filter(row -> row.New_Build == 1, Output_es_capacity)
+#power
+Output_power= CSV.read(input_dir*"power_hourly.csv",DataFrame) 
+Output_es_power =  CSV.read(input_dir*"es_power_hourly.csv",DataFrame) 
+
 
 #Aggregrate by technology
 All_agg_cap_df = aggregate_capdata(vcat(aggregate_capdata(Exist_capacity),aggregate_capdata(Exist_es_capacity),aggregate_capdata(New_capacity),aggregate_capdata(New_es_capacity)))
@@ -72,48 +64,33 @@ function fill_gendf_zero(df)
 end
 
 
-#All_agg_cap_df.Color =  map(x -> get(color_map, x, missing), All_agg_cap_df.Technology)
-#Exist_agg_cap_df.Color =  map(x -> get(color_map, x, missing), Exist_agg_cap_df.Technology)
+All_agg_cap_df.Color =  map(x -> get(color_map, x, missing), All_agg_cap_df.Technology)
+Exist_agg_cap_df.Color =  map(x -> get(color_map, x, missing), Exist_agg_cap_df.Technology)
 
-#re-order
+#re-order (not in use for now)
 #indices = sortperm([findfirst(x -> x == y, ordered_tech) for y in All_agg_cap_df.Technology])
 #All_agg_cap_reod_df =  All_agg_cap_df[indices,:]
 
-#plot capacity--------------------------------------------------------#
-#plot(All_agg_cap_df, kind="bar",x=:Zone, y=Symbol("Capacity (MW)"), marker_color=:Color, symbol=:Technology, Layout(title="Generation Capacity Mix at 2022", barmode="stack", xaxis_categoryorder="category ascending"))
-function plot_gen_mix(df::DataFrame, ordered_tech::Vector, color_map::Dict,tt::String)
-    agg_df = combine(groupby(df, [:Zone]),Symbol("Capacity (MW)") => sum)
-    rename!(agg_df, Symbol("Capacity (MW)_sum") => "Cap")
-    max_zone_cap = maximum(agg_df.Cap)
-
-    return plot([bar(x=sort(unique(df[:,:Zone])), y= sort(filter(row -> row.Technology == ordered_tech[i],df), :Zone)[:,"Capacity (MW)"], marker_color=color_map[ordered_tech[i]], name=ordered_tech[i] ) for i in 1:size(ordered_tech)[1]], 
-    Layout(title=tt, barmode="stack", 
-    xaxis_categoryorder="category ascending", xaxis_title_text="Regions",
-    yaxis_title_text="Capacity (MW)", yaxis_range=[0,1.1*max_zone_cap]))
-end
-plot_gen_mix(fill_gendf_zero(All_agg_cap_df), ordered_tech, color_map,  "Generation Capacity Mix at 2030")
-plot_gen_mix(fill_gendf_zero(Exist_agg_cap_df), ordered_tech, color_map,  "Generation Capacity Mix at 2022")
-#plot capacity--------------------------------------------------------#
 
 #plot power output --------------------------------------------------------#
 #aggregrated 
 
-#agg_zone_data = combine(groupby(Output_power, [:Technology]), names(Output_power, r"h\d+") .=> sum) ###
-#agg_es_dc_zone_data = combine(groupby(Output_es_power,[:Technology]), names(Output_es_power, r"dc_h\d+") .=> sum) ###
-#agg_es_soc_zone_data = combine(groupby(Output_es_power,[:Technology]), names(Output_es_power, r"soc_h\d+") .=> sum) ###
-#agg_es_c_zone_data = combine(groupby(Output_es_power,[:Technology]), names(Output_es_power, r"^c_h\d+") .=> sum) ###
+agg_zone_data = combine(groupby(Output_power, [:Technology]), names(Output_power, r"h\d+") .=> sum) 
+agg_es_dc_zone_data = combine(groupby(Output_es_power,[:Technology]), names(Output_es_power, r"dc_h\d+") .=> sum) 
+agg_es_soc_zone_data = combine(groupby(Output_es_power,[:Technology]), names(Output_es_power, r"soc_h\d+") .=> sum) 
+agg_es_c_zone_data = combine(groupby(Output_es_power,[:Technology]), names(Output_es_power, r"^c_h\d+") .=> sum) 
 
-```power_output_data_df = Dict(
+power_output_data_df = Dict(
     "agg_zone_data" =>agg_zone_data ,
     "agg_es_dc_zone_data"=>agg_es_dc_zone_data,
     "agg_es_soc_zone_data"=>agg_es_soc_zone_data,
     "agg_es_c_zone_data" => agg_es_c_zone_data 
-)```
+)
 
 hours=1:168
 ordered_tech_power = ["NuC","Coal","NGCC","NGCT","NG","Hydro","Oil","MSW","Bio","WindOn","WindOff","SolarPV","Hydro_pump","Battery","Other"]
 ordered_es_tech = ["Hydro_pump","Battery"]
-```function plot_power_output(data::Dict, ordered_tech_power::Vector,ordered_es_tech ::Vector, color_map::Dict,hours::UnitRange)
+function plot_power_output(data::Dict, ordered_tech_power::Vector,ordered_es_tech ::Vector, color_map::Dict,hours::UnitRange)
     agg_es_dc_zone_data=data["agg_es_dc_zone_data"]
     agg_es_c_zone_data=data["agg_es_c_zone_data"]
     agg_zone_data=data["agg_zone_data"]
@@ -143,7 +120,7 @@ ordered_es_tech = ["Hydro_pump","Battery"]
                 showlegend=true,
                 barmode="stack")
                 )
-end```
+end
 
 plot_power_output(power_output_data_df, ordered_tech_power, ordered_es_tech, color_map, hours)
 
